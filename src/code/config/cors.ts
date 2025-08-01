1b7ccc63407279364bc24af949e84a4e9a130d7f
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2025-08-01 20:10:40 +0700

    ADD CORS https://backend-takumifahri.vercel.app/

--- src/code/config/cors.ts
+++ src/code/config/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsOptions: cors.CorsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:5137'],
+    origin: ['http://localhost:3000', 'http://localhost:5137', 'https://backend-takumifahri.vercel.app/'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
