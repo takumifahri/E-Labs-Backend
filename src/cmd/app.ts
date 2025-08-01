621ca7b1441f0fde9676160ffc9658c04b87efb1
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2025-08-01 11:58:53 +0700

    add Unit test from vitest

--- src/cmd/app.ts
+++ src/cmd/app.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import router from "../code/routes/router";
+import corsOptions from "../code/config/cors";
+import cors from "cors";
 const app = express();
 app.use(express.json());
 
 
 // Index routes
-
+app.use(cors(corsOptions))
 app.use('/api', router);
 
 export default app;
