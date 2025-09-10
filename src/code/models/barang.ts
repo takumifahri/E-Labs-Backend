c7bc560c4c0048c258f482c73b8fd4843f1c1be5
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2025-09-10 08:46:40 +0700

    add dto for barang

--- src/code/models/barang.ts
+++ src/code/models/barang.ts
@@ -19,3 +19,30 @@ export interface Barang {
     updatedAt: Date;
     deletedAt?: Date;
 }
+
+export interface BarangRespone {
+    id: Number;
+    kode_barang: string;
+    nama_barang: string;
+    merek: string;
+    kondisi: string;
+    status: string;
+    kategori: Kategori_Barang;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date;
+}
+
+export interface BarangRequest {
+    id: Number;
+    kode_barang: string;
+    nama_barang: string; 
+    merek: string;
+    kondisi: string;
+    status: string;
+    kategori_id: number;
+    kategori: Kategori_Barang;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date;
+}
