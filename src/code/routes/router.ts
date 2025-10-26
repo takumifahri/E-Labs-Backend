2ba4bcc32bcddecf29413508a04f817118a9545f
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2025-10-27 00:20:28 +0700

    add routes log

--- src/code/routes/router.ts
+++ src/code/routes/router.ts
@@ -5,6 +5,7 @@ import authRouter from './auth/Auth';
 import master_ruangan_barang_router from './admin/master_ruangan_barang';
 import PeminjamanRouter from './user/Peminjaman';
 import verfikasi_router from './admin/verifikasiPeminjaman';
+import LogRouter from './user/logs';
 // import PlanRouter from './admin/plan';
 const router = express.Router();
 
@@ -12,12 +13,15 @@ const router = express.Router();
 router.use('/admin/users', AdminUserRouter);
 router.use('/admin/master', master_ruangan_barang_router);
 router.use('/admin/verifikasi', verfikasi_router);
+
 // router.use('/admin/plans', PlanRouter);
 router.use('/auth', authRouter);
-
-
 // Router Peminjaman Item
 router.use('/peminjaman', PeminjamanRouter);
+
+// Router Logs
+router.use('/logs', LogRouter);
+
 
 
 export default router;
